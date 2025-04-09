9803605c62233ee88acf4017ab2880c6c7c93375
kaushik Dutta <kaushik199906@gmail.com>
2025-04-10 02:13:55 +0530

    Docker installation steps

--- Contribute.md
+++ Contribute.md
@@ -15,4 +15,15 @@
 
 ## Docker installation
 
+-install docker
+-create a network - docker netwrok create user_project
+-start postgress
+
+- docker run --network user_project --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres
+  -build the image - `docker build --network=host  -t user-project .`
+  -start the image - `docker run -e DATABASE_URL=postgresql://postgres:mysecretpassword@localhost:5432/postgres --network user_project -p 3000:3000 user-project`
+
 ## Docker compose installation steps
+
+-install docker , docker-compose
+-run `docker-compose up`
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 
-app.get("/", (req, res) => {
-  const data = await PrismaClient.user.findMany();
+app.get("/", async (req, res) => {
+  const data = await prisma.user.findMany();
   res.json({
     data,
   });
 });
 
-app.post("/", (req, res) => {
-  await PrismaClient.user.create({
+app.post("/", async (req, res) => {
+  await prisma.user.create({
     data: {
       username: Math.random().toString(),
       password: Math.random().toString(),
